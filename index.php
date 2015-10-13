20eb83300f46ba19ae5d056df01a332610e26aa0
PHClaus <phhpro@gmail.com>
2015-10-12 20:18:21 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+    <head>
+        <title>Browser Edit Test Page</title>
+    </head>
+    <body>
+<?php include ('./load.php'); ?>
+        <h1>Browser Edit Test Page</h1>
+        <p>This is the virtual test page for Browser Edit.</p>
+        <p>It is auto-created each time the Browser Edit homepage is loaded.</p>
+        <p>The default password is <kbd>test</kbd>.</p>
+        <p><a href="?bedit" title="Open editor">Edit</a></p>
+    </body>
+</html>
