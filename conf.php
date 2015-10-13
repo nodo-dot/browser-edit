8ad8803a24e69d28e6a56b8b7cf80693ad0103ab
PHClaus <phhpro@gmail.com>
2015-10-12 20:15:53 -0500

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,42 @@
+<?php
+// trigger
+$bed_load = 'bedit';
+
+// password
+$bed_pass = 'bedit';
+
+// document root
+$bed_path = '/home/www/public_html';
+
+// script folder
+$bed_fold = '/browser-edit/';
+
+// login
+$bed_auth = $bed_fold . 'auth.php';
+
+// editor
+$bed_edit = $bed_fold . 'edit.php';
+
+// language
+$bed_lang = array ();
+
+// misc
+$bed_lang['name'] = 'Browser Edit';
+$bed_lang['home'] = 'Visit homepage';
+
+// load
+$bed_lang['pass'] = 'Enter password';
+$bed_lang['ok']   = 'OK';
+$bed_lang['post'] = 'Login to edit';
+$bed_lang['auth'] = 'Not authorised';
+
+// edit
+$bed_lang['file'] = 'File';
+$bed_lang['edit'] = 'Edit content. Make sure your mark-up is valid!';
+$bed_lang['undo'] = 'UNDO';
+$bed_lang['void'] = 'Reset all changes';
+$bed_lang['done'] = 'DONE';
+$bed_lang['save'] = 'Write changes to file and close editor. Make sure your mark-up is valid!';
+
+// make
+$bed_make = '20151012';
