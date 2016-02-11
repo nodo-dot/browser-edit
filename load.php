ad25c635d82723b6fbedaa9c30a0ebd4932ea4f5
PHClaus <phhpro@gmail.com>
2016-02-10 18:18:53 -0600

    Update load.php

--- load.php
+++ load.php
@@ -4,5 +4,6 @@ include ($_SERVER['DOCUMENT_ROOT'] . '/bedit/conf.php');
 
 // link file
 if (isset ($_GET[$bed_load])) {
-    header('Location: ' . $bed_auth . '?' . $_SERVER['PHP_SELF']);
+  header('Location: ' . $bed_auth . '?' . $_SERVER['PHP_SELF']);
+  exit;
 }
