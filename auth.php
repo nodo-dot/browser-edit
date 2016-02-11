27a4b6d38b6a7756fbf2383a31751f6a066333a9
PHClaus <phhpro@gmail.com>
2016-02-10 18:28:09 -0600

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,33 +1,24 @@
 <?php
 // load config
-require ('/home/www/public_html/bedit/conf.php');
+include ($_SERVER['DOCUMENT_ROOT'] . '/bedit/conf.php');
 
 // form submitted
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
+if (isset ($_POST['bed_submit'])) {
+  $bed_login = $_POST['bed_login'];
 
-    // link values
-    $bed_login = $_POST['bed_login'];
-
-    // check values
-    if ($bed_login == $bed_pass) {
-
-        // pass file to editor
-        header('Location: ' . $bed_edit . 
-               '?f=' . $_SERVER['QUERY_STRING']);
-
-    // invalid login
-    } else {
-        echo '<p>' . $bed_lang['auth'] . '</p>';
-    }
+  // check values and pass file to editor
+  if ($bed_login == $bed_pass) {
+    header('Location: ' . $bed_edit . '?f=' . $_SERVER['QUERY_STRING']);
+    exit;
+  } else {
+    echo '<p>' . $bed_lang['auth'] . '</p>';
+  }
 }
 ?>
 <form action="" method="POST">
-    <input name="bed_login" id="bed_login" 
-           size="12" maxlength="32" 
-           title="<?php echo $bed_lang['pass']; ?>" 
-           type="password">
-    <input value="<?php echo $bed_lang['ok']; ?>" 
-           title="<?php echo $bed_lang['post']; ?>" 
-           type="submit">
+  <input name="bed_login" size="12" maxlength="32" 
+         title="<?php echo $bed_lang['pass']; ?>" type="password">
+  <input name="bed_submit" value="<?php echo $bed_lang['ok']; ?>" 
+         title="<?php echo $bed_lang['post']; ?>" type="submit">
 </form>
 <?php echo $bed_file; ?>
