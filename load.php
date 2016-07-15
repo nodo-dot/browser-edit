f13178cd174cb8a40a3438b0c11a28ba0ba0f892
PHClaus <phhpro@gmail.com>
2016-07-15 14:08:04 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,9 +1,9 @@
 <?php
 // load config
-include ($_SERVER['DOCUMENT_ROOT'] . '/bedit/conf.php');
+include ($_SERVER['DOCUMENT_ROOT'] . '/browser-edit/conf.php');
 
 // link file
-if (isset ($_GET[$bed_load])) {
+if ($_SERVER['QUERY_STRING'] == $bed_load) {
   header('Location: ' . $bed_auth . '?' . $_SERVER['PHP_SELF']);
   exit;
 }
