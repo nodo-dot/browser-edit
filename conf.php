3c7a7757b3bd3a43df035112e6812e7457a8f186
PHClaus <phhpro@gmail.com>
2016-07-15 14:09:33 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,42 +1,11 @@
 <?php
-// trigger
+// set trigger, password, and script folder
 $bed_load = 'bedit';
-
-// password
 $bed_pass = 'bedit';
+$bed_fold = '/browser-edit/demo/';
 
-// document root
+// document root, login file, editor file, version
 $bed_path = $_SERVER['DOCUMENT_ROOT'];
-
-// script folder
-$bed_fold = '/bedit/';
-
-// login
 $bed_auth = $bed_fold . 'auth.php';
-
-// editor
 $bed_edit = $bed_fold . 'edit.php';
-
-// language
-$bed_lang = array ();
-
-// misc
-$bed_lang['name'] = 'Browser Edit';
-$bed_lang['home'] = 'Visit homepage';
-
-// load
-$bed_lang['pass'] = 'Enter password';
-$bed_lang['ok']   = 'OK';
-$bed_lang['post'] = 'Login to edit';
-$bed_lang['auth'] = 'Not authorised';
-
-// edit
-$bed_lang['file'] = 'File';
-$bed_lang['edit'] = 'Edit content.';
-$bed_lang['undo'] = 'UNDO';
-$bed_lang['void'] = 'Reset all changes';
-$bed_lang['done'] = 'DONE';
-$bed_lang['save'] = 'Write changes to file and close editor.';
-
-// make
-$bed_make = '20160210';
+$bed_make = '20160706';
