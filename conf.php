606ad9b33676894eaf1fb0ba9a351912acb8189b
PHClaus <phhpro@gmail.com>
2016-10-05 20:01:50 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,11 +1,8 @@
 <?php
-// set trigger, password, and script folder
-$bed_load = 'bedit';
-$bed_pass = 'bedit';
-$bed_fold = '/browser-edit/demo/';
-
-// document root, login file, editor file, version
-$bed_path = $_SERVER['DOCUMENT_ROOT'];
+$bed_path = '/home/www/htdocs';
+$bed_fold = '/site/bed/';
 $bed_auth = $bed_fold . 'auth.php';
 $bed_edit = $bed_fold . 'edit.php';
-$bed_make = '20160706';
+$bed_load = 'magic';
+$bed_pass = 'secret';
+$bed_make = '20161005';
