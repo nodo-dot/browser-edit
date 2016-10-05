c090ccb4ef4e04fdb3442913a25d62bc7dfb87a3
PHClaus <phhpro@gmail.com>
2016-10-05 20:02:50 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,8 +1,6 @@
 <?php
-// load config
-include ($_SERVER['DOCUMENT_ROOT'] . '/browser-edit/conf.php');
+include ('/path/to/bed/conf.php');
 
-// link file
 if ($_SERVER['QUERY_STRING'] == $bed_load) {
   header('Location: ' . $bed_auth . '?' . $_SERVER['PHP_SELF']);
   exit;
