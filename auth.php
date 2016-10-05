f02197d1f98720faa5b9523e6e61d3d87c3c8094
PHClaus <phhpro@gmail.com>
2016-10-05 20:01:19 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,22 +1,21 @@
 <?php
-// load config
-include ($_SERVER['DOCUMENT_ROOT'] . '/browser-edit/conf.php');
+include ('/path/to/bed/conf.php');
 
-// form submitted
-if (isset ($_POST['bed_submit'])) {
-  $bed_login = $_POST['bed_login'];
+if (isset ($_POST['bed_post'])) {
+  $bed_login = $_POST['bed_pass'];
 
-  // check values and pass file to editor
   if ($bed_login == $bed_pass) {
     header('Location: ' . $bed_edit . '?f=' . $_SERVER['QUERY_STRING']);
     exit;
   } else {
-    // invalid login
-    echo '<p>Not authorised!</p>';
+    echo '<p>Invalid login!</p>';
   }
 }
 ?>
 <form action="" method="POST">
-  <input name="bed_login" size="12" maxlength="32" title="Enter password" type="password">
-  <input name="bed_submit" value="Login" title="Login to edit" type="submit">
+  <div>
+    <label for="bed_pass">Password</label>
+    <input type="password" name="bed_pass" id="bed_pass" size="12" maxlength="32" title="Please enter your password">
+    <input type="submit" value="Login" title="Click here to login" name="bed_post">
+  </div>
 </form>
