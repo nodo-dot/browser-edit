d73fb78b52dcdabd046185d87a8c6cd88d5f0f00
PHClaus <phhpro@gmail.com>
2016-10-05 20:06:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php include ('./load.php'); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-    <head>
-        <title>Browser Edit Test Page</title>
-    </head>
-    <body>
-        <h1>Browser Edit Test Page</h1>
-        <p>This is the virtual test page for Browser Edit.</p>
-        <p>It is auto-created each time the Browser Edit homepage is loaded.</p>
-        <p>The default password is <kbd>bedit</kbd>.</p>
-        <p><a href="?bedit" title="Open editor">Edit</a></p>
-    </body>
+  <head>
+    <title>Browser Edit Test Page</title>
+  </head>
+  <body>
+    <h1>Browser Edit Test Page</h1>
+    <p>This is the Browser Edit test page.</p>
+    <p>The default token is <kbd>magic</kbd> and the password is <kbd>secret</kbd>.</p>
+    <p><a href="?magic" title="Open editor">Edit</a></p>
+  </body>
 </html>
