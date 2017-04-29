956b6d26782d86dec942a84a335c7e9264571c4d
PHClaus <phhpro@gmail.com>
2017-04-29 14:18:05 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,8 +1,20 @@
 <?php
-$bed_path = '/home/www/htdocs';
-$bed_fold = '/site/bed/';
+/*
+ * script folder
+ * login query
+ * login password
+ */
+$bed_fold = '/path/to/bed/';
+$bed_load = 'YOUR_AUTH_TOKEN';
+$bed_pass = 'YOUR_AUTH_PASSWORD';
+
+/*
+ * document root path
+ * login screen
+ * editor screen
+ * script version
+ */
+$bed_path = $_SERVER['DOCUMENT_ROOT'];
 $bed_auth = $bed_fold . 'auth.php';
 $bed_edit = $bed_fold . 'edit.php';
-$bed_load = 'magic';
-$bed_pass = 'secret';
-$bed_make = '20161005';
+$bed_make = '20170429';
