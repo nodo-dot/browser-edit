f34bd1592eccd247a836801c3f5dd37e06dfc53b
PHClaus <phhpro@gmail.com>
2017-04-29 14:18:57 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,9 +1,12 @@
 <?php
-include ('/path/to/bed/conf.php');
+//** load config
+include ($_SERVER['DOCUMENT_ROOT'] . '/path/to/bed/conf.php');
 
+//** form posted
 if (isset ($_POST['bed_post'])) {
   $bed_login = $_POST['bed_pass'];
 
+  //** check login password
   if ($bed_login == $bed_pass) {
     header('Location: ' . $bed_edit . '?f=' . $_SERVER['QUERY_STRING']);
     exit;
@@ -12,10 +15,10 @@ if (isset ($_POST['bed_post'])) {
   }
 }
 ?>
-<form action="" method="POST">
+<form action="#" method="POST" accept-charset="UTF-8">
   <div>
     <label for="bed_pass">Password</label>
-    <input type="password" name="bed_pass" id="bed_pass" size="12" maxlength="32" title="Please enter your password">
-    <input type="submit" value="Login" title="Click here to login" name="bed_post">
+    <input type="password" name="bed_pass" id="bed_pass" size="12" maxlength="32" title="Please enter your password" />
+    <input type="submit" name="bed_post" value="Login" title="Click here to login" />
   </div>
 </form>
