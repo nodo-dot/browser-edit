0fa12b3865656ecaf8402385006aabee57a24161
PHClaus <phhpro@gmail.com>
2017-05-15 20:29:31 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,9 +4,9 @@
  * login query
  * login password
  */
-$bed_fold = '/path/to/bed/';
-$bed_load = 'YOUR_AUTH_TOKEN';
-$bed_pass = 'YOUR_AUTH_PASSWORD';
+$bed_fold = "/path/to/bed/";
+$bed_load = "YOUR_ACCESS_TOKEN";
+$bed_pass = "YOUR_ACCESS_PASSWORD";
 
 /*
  * document root path
@@ -15,6 +15,6 @@ $bed_pass = 'YOUR_AUTH_PASSWORD';
  * script version
  */
 $bed_path = $_SERVER['DOCUMENT_ROOT'];
-$bed_auth = $bed_fold . 'auth.php';
-$bed_edit = $bed_fold . 'edit.php';
-$bed_make = '20170429';
+$bed_auth = $bed_fold . "auth.php";
+$bed_edit = $bed_fold . "edit.php";
+$bed_make = 20170515;
