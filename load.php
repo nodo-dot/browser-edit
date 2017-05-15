4101022cdca401034dae080d6115158bbc4c178a
PHClaus <phhpro@gmail.com>
2017-05-15 20:28:22 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,7 +1,9 @@
 <?php
-include ('/path/to/bed/conf.php');
+//** load config
+include ($_SERVER['DOCUMENT_ROOT'] . "/path/to/bed/conf.php");
 
-if ($_SERVER['QUERY_STRING'] == $bed_load) {
-  header('Location: ' . $bed_auth . '?' . $_SERVER['PHP_SELF']);
+//** check query
+if ($_SERVER['QUERY_STRING'] === $bed_load) {
+  header("Location: " . $bed_auth . "?" . $_SERVER['PHP_SELF']);
   exit;
 }
