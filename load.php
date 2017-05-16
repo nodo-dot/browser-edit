74f58ba0147e69055383da4a4e2c87d2f94dda71
PHClaus <phhpro@gmail.com>
2017-05-16 11:25:25 +0200

    Update load.php

--- load.php
+++ load.php
@@ -1,6 +1,6 @@
 <?php
 //** load config
-include ($_SERVER['DOCUMENT_ROOT'] . "/path/to/bed/conf.php");
+include ($_SERVER['DOCUMENT_ROOT'] . "/site/bed/conf.php");
 
 //** check query
 if ($_SERVER['QUERY_STRING'] === $bed_load) {
