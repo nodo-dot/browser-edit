425608c29324744aa50f0a5e266807174598b5f2
PHClaus <phhpro@gmail.com>
2017-05-19 13:19:15 +0200

    Update load.php

--- load.php
+++ load.php
@@ -4,6 +4,6 @@ include ($_SERVER['DOCUMENT_ROOT'] . "/site/bed/conf.php");
 
 //** check query
 if ($_SERVER['QUERY_STRING'] === $bed_load) {
-  header("Location: " . $bed_auth . "?" . $_SERVER['PHP_SELF']);
+  header("Location: " . $bed_auth . "?" . $_SERVER['SCRIPT_NAME']);
   exit;
 }
