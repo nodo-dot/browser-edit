c0acfb34d1e8dca1a9436ec89269e0fb7a88c32e
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-01-30 15:32:56 +0100

    20180130

---
+++ demo/test.php
@@ -0,0 +1,4 @@
+<h1>PHP Browser Edit Test Page</h1>
+<p></p><?php echo "Hello world!"; ?></p>
+<p>Hint: Move your pointer to the far left to show the tree list.</p>
+<p><a href="../" title="Click here to reload the demo">Try again</a></p>
