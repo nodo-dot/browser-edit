41aaede7497eff561e65a4fce82d85cc0b944ae3
PHClaus <15097539+phhpro@users.noreply.github.com>
2018-02-03 20:46:02 +0100

    20180203

---
+++ lang/de.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * @category  PHP_Editor
+ * @package   PHP_Browser_Edit
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/browser-edit
+ *
+ * Language file German
+ */
+
+
+//** Init strings array
+$bed_lstr = array();
+
+//** Strings
+$bed_lstr['empty_tree'] = "Leerer Baum! Bitte überprüfen Sie Ihre Einstellungen.";
+$bed_lstr['pass']       = "Passwort";
+$bed_lstr['pass_text']  = "Tippen Sie hier um Ihr Passwort einzugeben";
+$bed_lstr['enter']      = "Anmelden";
+$bed_lstr['enter_text'] = "Klicken Sie hier um sich anzumelden";
+$bed_lstr['deleted']    = "GELÖSCHT -- Sichern zum wiederherstellen";
+$bed_lstr['get_script'] = "Klicken Sie hier um eine kostenlose Kopie dieses Skriptes zu erhalten.";
+$bed_lstr['file']       = "Datei:";
+$bed_lstr['edit_text']  = "Klicken Sie hier um zu editieren";
+$bed_lstr['left']       = "DATEI";
+$bed_lstr['left_text']  = "Bewegen Sie den Zeiger über den linken Rand um den Dateibaum anzuzeigen.";
+$bed_lstr['right']      = "ANSICHT";
+$bed_lstr['right_text'] = "Bewegen Sie den Zeiger über den rechten Rand um die Dateivorschau anzuzeigen.";
+$bed_lstr['quit']       = "Ende";
+$bed_lstr['quit_text']  = "Klicken Sie hier um den Editor zu beenden.";
+$bed_lstr['del']        = "Löschen";
+$bed_lstr['del_text']   = "Klicken Sie hier um diese datei zu löschen. Die Inhalte bleiben vorerst im Speicher. Versehentlich gelöschte Dateien können durch Druck auf Sichern wiederhergestellt werden.";
+$bed_lstr['file_text']  = "Tippen Sie hier um den Namen der neuen Datei eizugeben. Sie können auch einen Pfad angebeben (ohne führenden /, z.B. foo/bar/baz.txt) um die Datei an einem anderen Ort anzulegen.";
+$bed_lstr['copy']       = "Kopieren";
+$bed_lstr['copy_text']  = "Klick Sie hier um die datei zu kopieren.";
+$bed_lstr['move']       = "Name";
+$bed_lstr['move_text']  = "Klicken Sie hier um die Datei zu bewegen (umzubenennen).";
+$bed_lstr['new']        = "Neu";
+$bed_lstr['new_text']   = "Klicken Sie hier um eine neue Datei anzulegen.";
+$bed_lstr['save']       = "Sichern";
+$bed_lstr['save_text']  = "Klicken Sie hier um allen Änderungen zu speichern oder eine versehentlich gelöschte Datei wiederherzustellen.";
---
+++ lang/es.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * PHP Version 5 and above
+ *
+ * @category  PHP_Editor
+ * @package   PHP_Browser_Edit
+ * @author    P H Claus <phhpro@gmail.com>
+ * @copyright 2015 - 2018 P H Claus
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ * @version   GIT: Latest
+ * @link      https://github.com/phhpro/browser-edit
+ *
+ * Language file Spanish
+ */
+
+
+//** Init strings array
+$bed_lstr = array();
+
+//** Strings
+$bed_lstr['empty_tree'] = "Árbol vacío! Por favor, compruebe su configuración.";
+$bed_lstr['pass']       = "Contraseña";
+$bed_lstr['pass_text']  = "Escriba aquí para ingresar su contraseña";
+$bed_lstr['enter']      = "Entrar";
+$bed_lstr['enter_text'] = "Haga clic aquí para ingresar";
+$bed_lstr['deleted']    = "ELIMINADO - Guardar para restaurar";
+$bed_lstr['get_script'] = "Haga clic aquí para obtener una copia gratuita de este script.";
+$bed_lstr['file']       = "Archivo:";
+$bed_lstr['edit_text']  = "Haga clic aquí para editar";
+$bed_lstr['left']       = "ARCHIVO";
+$bed_lstr['left_text']  = "Desplace el margen izquierdo para ver el árbol de archivos.";
+$bed_lstr['right']      = "VISTA";
+$bed_lstr['right_text'] = "Desplace el margen derecho para obtener una vista previa del archivo.";
+$bed_lstr['quit']       = "Dejar";
+$bed_lstr['quit_text']  = "Haga clic aquí para salir del editor.";
+$bed_lstr['del']        = "Borrar";
+$bed_lstr['del_text']   = "Haga clic aquí para eliminar este archivo. Su contenido permanecerá en la memoria por el momento. Los archivos borrados accidentalmente se pueden restaurar presionando el botón Guardar.";
+$bed_lstr['file_text']  = "Escriba aquí para ingresar el nombre del nuevo archivo. Puede agregar una ruta (por ejemplo, foo / bar / baz.txt) para crear el archivo en una ubicación diferente.";
+$bed_lstr['new']        = "Nuevo";
+$bed_lstr['new_text']   = "Haga clic aquí para crear un nuevo archivo.";
+$bed_lstr['copy']       = "Copiar";
+$bed_lstr['copy_text']  = "Haga clic aquí para copiar el archivo.";
+$bed_lstr['move']       = "Mover";
+$bed_lstr['move_text']  = "Haga clic aquí para mover (cambiar el nombre) del archivo.";
+$bed_lstr['save']       = "Guardar";
+$bed_lstr['save_text']  = "Haga clic aquí para guardar todos los cambios o restaurar un archivo borrado accidentalmente.";
